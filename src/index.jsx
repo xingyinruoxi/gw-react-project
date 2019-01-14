b5b172b6f9cf9e90babb2e42fbc0580dadc882da
周林 <634023699@qq.com>
2019-01-14 14:06:52 +0800

    修改后缀名

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {Router} from 'react-router-dom'
-import { store ,history} from '@/store';
+import { Router } from 'react-router-dom';
+import { store, history } from '@/store';
 import App from '@/App';
 import '@/common/less/base.less';
 // const history = syncHistoryWithStore(browserHistory, store)
 
 import * as serviceWorker from './serviceWorker';
+
 function render() {
 	ReactDOM.render(
 		<Provider store={store}>
 			<Router history={history}>
-				<App/>
-			</Router>
+				<App />
+			</Router>{' '}
 		</Provider>,
 		document.getElementById('root')
 	);
